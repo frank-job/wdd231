225a700e01a926869a180496ea48c601c20737d6
frank-job <Jobf0837@gmail.com>
2026-01-27 23:49:02 -0800

    week 4 errors cleaned

--- chamber/scripts/thankyou.js
+++ chamber/scripts/thankyou.js
@@ -1,11 +1,11 @@
 
-        // 1. Grab the current URL
+      
         const currentUrl = window.location.href;
 
-        // 2. Split the URL into parts
+      
         const everything = currentUrl.split('?');
 
-        // 3. Grab just the data part (if it exists)
+     
         if (everything.length > 1) {
             const formData = everything[1].split('&');
 
@@ -14,10 +14,10 @@
             function show(cup) {
                 formData.forEach((element) => {
                     if (element.startsWith(cup)) {
-                        // Clean up the text (remove %20, +, etc)
+                      
                         let result = element.split('=')[1].replace(/%40/g, "@").replace(/\+/g, " ").replace(/%3A/g, ":");
                         
-                        // Create HTML
+                   
                         let item = document.createElement('p');
                         item.innerHTML = `<strong>${cup}:</strong> ${result}`;
                         showInfo.appendChild(item);
@@ -25,7 +25,7 @@
                 });
             }
 
-            // Call the function for the specific fields required
+           
             show("firstName");
             show("lastName");
             show("email");
@@ -34,3 +34,5 @@
             show("timestamp");
         }
   
+
+  document.getElementById('timestamp').value = new Date().toLocaleString();
